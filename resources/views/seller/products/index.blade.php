5d3437ea48ab135a99483fb6fdbb3e1b1269a981
Thquan105 <thquan105@gmail.com>
2024-01-07 21:15:23 +0700

    Show product

--- resources/views/seller/products/index.blade.php
+++ resources/views/seller/products/index.blade.php
@@ -1,102 +1,135 @@
 @extends('layouts.seller.app')
 
 @section('content')
-
     <!-- Main content -->
     <section class="content pt-4">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
 
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Dữ liệu Sản phẩm</h3>
-                <a href="{{ route('seller.products.create')}}" class="btn btn-success shadow-sm float-right"> <i class="fa fa-plus"></i> Thêm </a>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <div class="table-responsive">
-                    <table id="data-table" class="table table-bordered table-striped">
-                      <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Danh mục</th>
-                            <th>Ảnh</th>
-                            <th>Thao tác</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse($products as $product)
-                            <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ $product->data()['name'] }}</td>
-                                <td>
-                                    @if (isset($product->data()['photo']))
-                                        <a href="{{ $product->data()['photo'] }}" target="_blank">
-                                            <img src="{{ $product->data()['photo'] }}" width="45px"
-                                                height="45px" alt="">
-                                        </a>
-                                    @else
-                                        <span class="badge badge-warning">No image</span>
-                                    @endif
-                                </td>
-                                <td>
-                                    <div class="btn-group btn-group-sm">
-                                        <a href="{{ route('admin.subcategories.index', $product->id()) }}"
-                                            class="btn btn-sm btn-success mr-4">
-                                            <i class="fa fa-eye">{{ __(' Xem Subcategory') }}</i>
-                                        </a>
-                                        <a href="{{ route('admin.categories.edit', $product->id()) }}"
-                                            class="btn btn-sm btn-primary mr-4">
-                                            <i class="fa fa-edit">{{ __(' Sửa') }}</i>
-                                        </a>
-                                        <form onclick="return confirm('Chắc chắn xóa ?')"
-                                            action="{{ route('admin.categories.destroy', $product->id()) }}"
-                                            method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn btn-sm btn-danger" type="submit"><i
-                                                    class="fa fa-trash">{{ __(' Xóa') }}</i></button>
-                                        </form>
-                                    </div>
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="4" class="text-center">Trống !</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                    </table>
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Dữ liệu Sản phẩm</h3>
+                            <a href="{{ route('seller.products.create') }}" class="btn btn-success shadow-sm float-right"> <i
+                                    class="fa fa-plus"></i> Thêm </a>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table id="data-table" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Ảnh</th>
+                                            <th>Tên</th>
+                                            <th>Ngành hàng</th>
+                                            <th>Danh mục</th>
+                                            <th>Giá</th>
+                                            <th>Kho hàng</th>
+                                            <th>Thao tác</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @forelse($products as $product)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                @php
+                                                    $productRef = app('firebase.firestore')
+                                                        ->database()
+                                                        ->collection('product')
+                                                        ->document($product->id());
+                                                    $imgProduct = $productRef->collection('image')->documents();
+                                                    $category = app('firebase.firestore')
+                                                        ->database()
+                                                        ->collection('user')
+                                                        ->document($product->data()['idShop'])
+                                                        ->collection('categoryShop')
+                                                        ->document($product->data()['idSubCategory'])
+                                                        ->snapshot();
+                                                    $attributes = $productRef->collection('option')->documents();
+                                                    $minPrice = PHP_INT_MAX;
+                                                    $maxPrice = 0;
+                                                    $totalQuantity = 0;
+                                                    foreach ($attributes as $attribute) {
+                                                        if ($minPrice >= $attribute->data()['price']) {
+                                                            $minPrice = $attribute->data()['price'];
+                                                        }
+                                                        if ($maxPrice <= $attribute->data()['price']) {
+                                                            $maxPrice = $attribute->data()['price'];
+                                                        }
+                                                        $totalQuantity += $attribute->data()['quantity'];
+                                                    }
+                                                @endphp
+                                                <td>
+                                                    @if (isset($imgProduct))
+                                                        @foreach ($imgProduct as $item)
+                                                            <a href="{{ $item->data()['url'] }}" target="_blank">
+                                                                <img src="{{ $item->data()['url'] }}" width="45px"
+                                                                    height="45px" alt="">
+                                                            </a>
+                                                        @endforeach
+                                                    @else
+                                                        <span class="badge badge-warning">No image</span>
+                                                    @endif
+                                                </td>
+                                                <td>{{ $product->data()['name'] ?? 'none' }}</td>                                                
+                                                <td>{{ $category->data()['name'] ?? 'none' }}</td>
+                                                <td>{{ $category->data()['name'] ?? 'none' }}</td>
+                                                @if ($attributes->size() == 1) 
+                                                    <td>{{ $minPrice }}</td>
+                                                @else
+                                                    <td>{{ $minPrice }} - {{ $maxPrice }}</td>
+                                                @endif                                               
+                                                <td>{{ $totalQuantity }}</td>
+                                                <td>
+                                                    <div class="btn-group btn-group-sm">
+                                                        <a href="{{ route('admin.categories.edit', $product->id()) }}"
+                                                            class="btn btn-sm btn-primary mr-1">
+                                                            <i class="fa fa-edit">{{ __(' Sửa') }}</i>
+                                                        </a>
+                                                        <form onclick="return confirm('Chắc chắn xóa ?')"
+                                                            action="{{ route('admin.categories.destroy', $product->id()) }}"
+                                                            method="POST">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button class="btn btn-sm btn-danger" type="submit"><i
+                                                                    class="fa fa-trash">{{ __(' Xóa') }}</i></button>
+                                                        </form>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td colspan="7" class="text-center">Trống !</td>
+                                            </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
                 </div>
-              </div>
-              <!-- /.card-body -->
+                <!-- /.col -->
             </div>
-            <!-- /.card -->
-          </div>
-          <!-- /.col -->
+            <!-- /.row -->
         </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container-fluid -->
+        <!-- /.container-fluid -->
     </section>
     <!-- /.content -->
 @endsection
 
 @push('style-alt')
-  <!-- DataTables -->
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css">
 @endpush
 
-@push('script-alt') 
-    <script
-        src="https://code.jquery.com/jquery-3.6.3.min.js"
-        integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
-        crossorigin="anonymous"
-    >
-    </script>
+@push('script-alt')
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js"
+        integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
     <script>
-    $("#data-table").DataTable();
+        $("#data-table").DataTable();
     </script>
 @endpush
